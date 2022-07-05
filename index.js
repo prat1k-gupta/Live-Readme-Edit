2a7fe60ed12ff8337d85fd05a5b95e69fde41fc6
prat1k-gupta <pratikgupta9773@gmail.com>
2022-07-05 19:09:01 +0530

    new update

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-const express = require("express");
-const app = express();
 const notion2md = require("./notion2md")
-// var multer  = require('multer')
-// var notion2md = multer({ dest: './notion2md' })
+
+//calling notion2md
 notion2md(); 
 
-// or
-// import {NotionToMarkdown} from "notion-to-md";
 
